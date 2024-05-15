ba6820664d433d0eaab9595b0d9443dd2aa95741
Matias Nahuel Heredia <matiasnahuelheredia@gmail.com>
2024-05-15 01:01:16 -0300

    agregar imagen en card

--- static/js/api.js
+++ static/js/api.js
@@ -7,8 +7,25 @@ fetch('./static/js/api.json', {
     }
     return response.json();
   })
-  .then(data => {
-    console.log(data);
+  .then(cardsData => {
+    
+    const container = document.getElementById("container");
+    const firstTen = cardsData.data.slice(1,1000);   
+    console.log(firstTen); 
+    const characterCardHTML = firstTen.map(card => {
+      return `
+      <div class="card" id="firstCard">
+      <img src="${card.image}">
+      <h4>${card.name}</h4>
+      <h4>Lugar :${card.location.name}</h4>
+      <div>${card.description}</div>
+      <a href="${card.image}">Leer más</a>
+      </div>
+      `;
+    }).join('');
+    container.innerHTML = characterCardHTML;
+
+
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
