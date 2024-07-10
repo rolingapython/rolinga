53cf1d873cd889678046103bbc51183ce8c00ac5
Matias Nahuel Heredia <matiasnahuelheredia@gmail.com>
2024-07-10 08:19:46 -0300

    agregar carga de comentarios

--- init.sql
+++ init.sql
@@ -3,14 +3,14 @@ CREATE TABLE events (
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255),
     description TEXT,
-    image TEXT,
+    image LONGTEXT,
     link VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS News (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    image TEXT,
+    image LONGTEXT,
     description TEXT,
     link VARCHAR(255),
     publisher VARCHAR(255)
