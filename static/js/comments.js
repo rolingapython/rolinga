fb8ecad92e10f8c5fa140d5386890f63925429b6
Matias Nahuel Heredia <matiasnahuelheredia@gmail.com>
2024-07-10 08:22:44 -0300

    pasar comentarios.js a javascript

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,33 +1,41 @@
 document.getElementById('create-event-form').addEventListener('submit', function(e) {
     e.preventDefault();
     
-    const name = document.getElementById('name').value;
-    const location = document.getElementById('location').value;
-    const description = document.getElementById('description').value;
-    const image = document.getElementById('image').value;
-    const link = document.getElementById('link').value;
-    
-    const eventData = {
-        name: name,
-        location: location,
-        description: description,
-        image: image,
-        link: link
+    const fileInput = document.getElementById('image');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        console.error('No se seleccionó ningún archivo.');
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        const data = {
+            name: document.getElementById('name').value,
+            location: document.getElementById('location').value,
+            description: document.getElementById('description').value,
+            image: event.target.result, 
+            link: document.getElementById('link').value
+        };        
+        console.log(data);         
+        fetch('/api/createnews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); 
+            
+        })
+        .catch(error => {
+            console.error('Error al enviar los datos:', error);            
+        });
     };
-    console.log(JSON.stringify(eventData));
-    fetch('/api/createnews', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('message').innerText = data.message;
-        document.getElementById('create-event-form').reset();
-    })
-    .catch(error => {
-        document.getElementById('message').innerText = 'Ocurrió un error al crear el evento';
+
+    reader.readAsDataURL(file); 
     });
-});
